57e3b142ebe51eb840759afbbc854b97e8c3fe6a
parth123965-wq <parth123965@gmail.com>
2026-09-03 17:18:56 +0530

    Add Dockerfiles, Docker Compose, fix requirements.txt and CORS settings

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -14,8 +14,16 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500"
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://localhost",
+        "http://localhost:80",
+        "http://127.0.0.1:80",
+        "http://127.0.0.1",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
     ],
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=[
         "*"
